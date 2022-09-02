d16b3df69c8d5500a3161919b61ff87f6fff521c
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2022-09-02 22:46:42 +0530

    V2

--- rock_scissor_paper.py
+++ rock_scissor_paper.py
@@ -3,12 +3,17 @@
 # under the function get_choices both statment is intended with same
 import random
 
-def check_wins(choices):
-  if (choices["player"] == "rock" and choices["computer"] != "paper"):
+def check_win(choices):
+  # print("You chose " + choices["player"] + " computer chose " + choices["computer"])
+  print(f"You chose {choices['player']} and computer chose {choices['computer']}")
+
+  if choices["player"] == choices["computer"]:
+    return "Tie"
+  elif choices["player"] == "rock" and choices["computer"] == "scissors":
     return "Player wins"
-  elif(choices["player"] == "scissors" and choices["computer"] != "rock"):
+  elif choices["player"] == "scissors" and choices["computer"] == "paper":
     return "Player wins"
-  elif(choices["player"] == "paper" and choices["computer"] != "scissors"):
+  elif choices["player"] == "paper" and choices["computer"] == "rock":
     return "Player wins"
   else:
     return "Computer wins"
@@ -20,7 +25,7 @@ def get_choices():
 
   choices = {"player": player_choice, "computer": computer_choice}
 
-  return check_wins(choices)
+  return check_win(choices)
   
 
 result = get_choices()
